2a4126a728c803c23c4ebe7c7f20190ec6bfccd7
B4TRUH4 <matveybond2002@gmail.com>
2024-11-09 22:29:54 +0700

    add: jwt and password utils

--- auth_service/src/config.py
+++ auth_service/src/config.py
@@ -13,4 +13,11 @@ class Settings(BaseSettings):
     MODE: str
 
 
+class JWTSettings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+
 settings = Settings()
+jwt_settings = JWTSettings()
