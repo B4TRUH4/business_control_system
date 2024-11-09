263c77cc88c1bddbd359bb2f1704d7ba9292774c
B4TRUH4 <matveybond2002@gmail.com>
2024-11-09 22:50:15 +0700

    feat: register api router

--- auth_service/src/main.py
+++ auth_service/src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
+from src.api.exceptions import (
+    ServiceException,
+)
 from src.config import settings
-from src.metadata import TITLE, DESCRIPTION, VERSION
+from src.metadata import TITLE, DESCRIPTION, VERSION, TAG_METADATA
+from src.api import router
+from src.schemas.response import ErrorResponse
 
 
 def create_fastapi_app() -> FastAPI:
@@ -9,17 +16,20 @@ def create_fastapi_app() -> FastAPI:
         fastapi_app = FastAPI(
             title=TITLE,
             description=DESCRIPTION,
+            openapi_tags=TAG_METADATA,
             version=VERSION,
             docs_url=None,
             redoc_url=None,
+
         )
     else:
         fastapi_app = FastAPI(
             title=TITLE,
             description=DESCRIPTION,
             version=VERSION,
+            openapi_tags=TAG_METADATA,
         )
-
+    fastapi_app.include_router(router, prefix='/api')
     return fastapi_app
 
 
