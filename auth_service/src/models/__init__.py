7e7f672aded9ecb0dcb32a7a9e93170bb0ee698c
B4TRUH4 <matveybond2002@gmail.com>
2024-11-09 22:31:19 +0700

    fix: add __init__.py file in src/models

--- auth_service/src/models/__init__.py
+++ auth_service/src/models/__init__.py
@@ -1,5 +1,17 @@
 __all__ = [
     'Base',
+    'User',
+    'Company',
+    'Member',
+    'Account',
+    'Invite',
+    'Secret',
 ]
 
 from .base import Base
+from .user import User
+from .company import Company
+from .members import Member
+from .account import Account
+from .invite import Invite
+from .secret import Secret
